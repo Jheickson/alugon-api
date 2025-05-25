3cb32b57984a4a969833db69270e44f8975b4bf1
Jheickson <felipesantosatm@outlook.com>
2025-05-24 21:40:43 -0300

    Ajuste no CORS

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,14 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true
+  })
+);
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -14,13 +21,6 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(router);
 
-app.use(
-  cors({
-    origin: "https://localhost:3000", // Permitir apenas este domínio
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
 // Rota para a documentação do Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
